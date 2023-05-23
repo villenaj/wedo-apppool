00b7513be572b588f5d746061bb2cd930a6e9f1e
Joel Villena <93912972+villenaj@users.noreply.github.com>
2023-05-23 18:42:49 +0800

    Update applicant.js

--- public/js/applicant.js
+++ public/js/applicant.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
+    loadprovince()
+
     $(document).on('click', '#btnSubmit', function (e) {
-
-
         var datas = $('#frmApplicant');
         var formData = new FormData($(datas)[0]);
 
@@ -19,7 +19,7 @@ $(document).ready(function () {
                 if (response.data.status == 200) {
                     $('span.error-text').text("");
                     $('input.border').removeClass('border border-danger');
-                    // $('#frmApplicant')[0].reset();
+                    $('#frmApplicant')[0].reset();
                     swal("Good job!", response.data.msg, "success");
                 }
                 //error
@@ -33,8 +33,130 @@ $(document).ready(function () {
 
             })
             .then(function () { });
-
     });
 
+    //JS
+    //load province
+    function loadprovince() {
+        axios.get('/load_province')
+            .then(function (response) {
+                var htmlData = '';
+                htmlData += ("<option value=0>-</option>");
+                $(response.data.data).each(function (index, row) {
+                    htmlData += ("<option value=" + row.provDesc + ">" + row.provDesc + "</option>");
+                })
+                // $("#tblHome").empty().append(htmlData);
+                $("#province").empty();
+                $("#province").append(htmlData);
+            })
+            .catch(function (error) {
+                dialog.alert({
+                    message: error
+                });
+            })
+            .then(function () { });
+    }
+
+    // //province
+    $(document).on('change', '#province ', function (e) {
+        var provCode = $(this).val();
+
+        $("#specsdsc_details").val('');
+        $('span.error-text').text("");
+        $('input.border').removeClass('border border-danger');
+    })
+
+    // PROVINCE
+    $(document).on('change', '#province ', function (e) {
+        var provCode = $(this).val();
+        axios.get('/onselect_province_load_city', {
+            params: {
+                id: provCode
+            }
+        })
+            // axios.get('/onselect_province_load_city')
+            .then(function (response) {
+                var htmlData = '';
+                htmlData += ("<option value=0>-</option>");
+
+                $(response.data.data).each(function (index, row) {
+                    htmlData += ("<option value=" + row.citymunDesc + ">" + row.citymunDesc + "</option>");
+                })
+                // $("#tblHome").empty().append(htmlData);
+                $("#city").empty();
+                $("#city").append(htmlData);
+            })
+            .catch(function (error) {
+                dialog.alert({
+                    message: error
+                });
+            })
+            .then(function () { });
+    });
+
+      // CITY
+    $(document).on('change', '#city ', function(e) {
+        var citycode = $(this).val();
+        axios.get('/onselect_city_load_brgy', {
+            params: {
+                id: citycode
+            }
+        })
+        // axios.get('/onselect_province_load_city')
+        .then(function (response) {
+            var htmlData='';
+            htmlData += ("<option value=0>-</option>");
+
+            $(response.data.data).each(function(index, row) {
+                htmlData += ("<option value=" + row.brgyDesc + ">" + row.brgyDesc + "</option>");
+            })
+            // $("#tblHome").empty().append(htmlData);
+            $("#barangay").empty();
+            $("#barangay").append(htmlData);
+        })
+        .catch(function (error) {
+            dialog.alert({
+                message: error
+            });
+        })
+        .then(function () {});
+    });
+
+
 });
+
+
+
+
+
+
+
+
+// // CITY
+// $(document).on('change', '#city ', function (e) {
+//     var citycode = $(this).val();
+//     axios.get('/onselect_city_load_brgy', {
+//         params: {
+//             id: citycode
+//         }
+//     })
+//         // axios.get('/onselect_province_load_city')
+//         .then(function (response) {
+//             var htmlData = '';
+//             htmlData += ("<option value=0>-</option>");
+
+//             $(response.data.data).each(function (index, row) {
+//                 htmlData += ("<option value=" + row.brgyCode + ">" + row.brgyDesc + "</option>");
+//             })
+//             // $("#tblHome").empty().append(htmlData);
+//             $("#barangay").empty();
+//             $("#barangay").append(htmlData);
+//         })
+//         .catch(function (error) {
+//             dialog.alert({
+//                 message: error
+//             });
+//         })
+//         .then(function () { });
+// });
 
